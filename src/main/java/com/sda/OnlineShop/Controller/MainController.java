8502b3706a85f0ffc21f998e94b23f4d6ca3d372
Sergiu Marandescu <sergiu.marandescu@gmail.com>
2023-02-15 16:57:24 +0200

    Initial project skeleton

---
+++ src/main/java/com/sda/OnlineShop/Controller/MainController.java
@@ -0,0 +1,20 @@
+package com.sda.OnlineShop.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/addProduct")
+    public String addProductGet() {
+        return "addProduct";            // here we add the view name from spring MVC
+    }
+
+    @PostMapping("/addProduct")
+    public String addProductPost() {
+        System.out.println("addProduct was called!");
+        return "addProduct";
+    }
+}
+
