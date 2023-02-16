ef8a46dc2164b1f4800e6681efa144a99fb3d3c3
Sergiu Marandescu <sergiu.marandescu@gmail.com>
2023-02-16 21:07:25 +0200

    implemented home page si image

--- src/main/java/com/sda/OnlineShop/Controller/MainController.java
+++ src/main/java/com/sda/OnlineShop/Controller/MainController.java
@@ -8,23 +8,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 public class MainController {
     @Autowired
     private ProductService productService;
+
     @GetMapping("/addProduct")
     public String addProductGet(Model model) {
         ProductDto productDto = new ProductDto();
-        model.addAttribute("productDto",productDto);
+        model.addAttribute("productDto", productDto);
         return "addProduct";            // here we add the view name from spring MVC
     }
 
     @PostMapping("/addProduct")
-    public String addProductPost(@ModelAttribute ProductDto productDto) {
-        productService.addProduct(productDto);
+    public String addProductPost(@ModelAttribute ProductDto productDto,
+                                 @RequestParam("productImage") MultipartFile productImage) {
+        productService.addProduct(productDto, productImage);
         System.out.println(productDto);
         return "addProduct";
+    }
+
+    @GetMapping("home")
+    public String homeGet(Model model) {
+        List<ProductDto> productDtos = productService.getAllProductDtos();
+        model.addAttribute("productDtos", productDtos);
+        return "home";
     }
 }
 
